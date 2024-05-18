56d2db2fa1307e9456745dabdddf92ed2ff619a6
TOM SABU <tomsabu0000@gmail.com>
2024-05-18 16:34:56 +0530

    "Moved image files to /images directory, updated import paths, and removed deleted files"

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './LoginRegister.css';
+import './styles/LoginRegister.css';
 
 import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
